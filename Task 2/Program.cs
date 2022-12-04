d5f34525727f083fea9416494fbfdd9696efad14
Evgen <beu08@list.ru>
2022-12-04 16:41:35 +0600

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет. 
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Promt(string massage)
+{
+    System.Console.Write($"{massage} >");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+ int A = Promt("Ваше число =>" + " ");
+
+ int number = A % 10;
+ int number1 = A / 100;
+
+ if(number1 == 0)
+ {
+       Console.WriteLine("Третьей цифры в вашем числе нет");
+ }
+ else
+ {
+    if(number >= 0)
+    {
+        Console.WriteLine("Третья цифра вашего числа" + " " + number);
+    }
+    
+ }
